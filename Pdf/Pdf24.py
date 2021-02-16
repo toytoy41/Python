07ef17694e4dcc45eac77f2391ee746e343c5b15
HP-2018\toyto <toytoy@da2.so-net.ne.jp>
2021-02-16 12:41:47 +0900

    整理２

--- Pdf/Pdf24.py
+++ Pdf/Pdf24.py
@@ -1,7 +1,8 @@
 import os
-import pathlib
+# import pathlib
 from pathlib import Path
 import PyPDF2
+
 
 class Pdf24:
 
@@ -10,64 +11,40 @@ class Pdf24:
         pass
 
     def make_pdf24_dir2dir(self, indir, outdir):
-        '''
+        """
         indirにあるすべてのPDFファイルを変換する。
         :param indir: PDFのもとファイルがあるディレクトリー
         :param outdir: 出力用ディレクトリー
         :return:
-        '''
+        """
         for f in list(Path(indir).glob('./*.pdf')):
             outfile = outdir + '\\' + self.infile2outfile(f)
             self.make_pdf24_file2file(f, outfile)
 
     def infile2outfile(self, infile):
-        infile_name = os.path.basename(infile).split('\.pdf')[0]
-        outfilename = infile_name  + '_pdf24.pdf'
+        infile_name = os.path.basename(infile).split('.pdf')[0]
+        outfilename = infile_name + '_pdf24.pdf'
         return outfilename
 
-    # def get_path_fname(self, infile):
-    #     '''
-    #     ロングファイル名から、ディレクトリーをファイル名を返す
-    #     :param infile: ロングファイル名
-    #     :return: ディレクトリーをファイル名
-    #     '''
-    #     dir_name = os.path.dirname(infile)
-    #     file_name = os.path.basename(infile).split('.pdf')[0]
-    #     return dir_name, file_name
-
-    # def make_pdf24_file2dir(self, infname, outdir):
-    #     '''
-    #     infname=PDFファイルをoutdirに書き込む
-    #     :param infname:
-    #     :param outdir:
-    #     :return:
-    #     '''
-    #     items = self.get_path_fname(infname)
-    #     indir = items[0]
-    #     filename= items[1] + '.pdf'
-    #     print(indir ,' ', filename)
-    #     outfilename = items[1].split('.')[0]  + '_pdf24.pdf'
-    #
-    #     self.make_pdf24_file2file(indir, outdir, filename, outfilename)
-
     def make_pdf24_file2file(self, infname, outfname):
-        '''
+        """
         一つのPDFファイルを、outfname名で変換する
         :param infname:
         :param outfname:
         :return:
-        '''
+        """
+        print(outfname)
         if os.path.exists(outfname):
             os.remove(outfname)
 
         pdf_reader = PyPDF2.PdfFileReader(infname)
         pdf_writer = PyPDF2.PdfFileWriter()
 
-        lastNum = pdf_reader.getNumPages()
+        lastnum = pdf_reader.getNumPages()
 
-        for i in range(0, lastNum, 2):
-            if i >= lastNum - 1:
-                if lastNum % 2 == 0:
+        for i in range(0, lastnum, 2):
+            if i >= lastnum - 1:
+                if lastnum % 2 == 0:
                     pdf_writer.addPage((pdf_reader.getPage(i + 1)))
                     pdf_writer.addPage((pdf_reader.getPage(i)))
                 else:
@@ -82,50 +59,15 @@ class Pdf24:
 
         pass
 
-    # def make_pdf24file(self, indir, outdir, infname, outfname):
-    #
-    #     # os.chdir(inPath)
-    #     outfname = outdir + '\\' + outfname
-    #     if os.path.exists(outfname):
-    #         os.remove(outfname)
-    #     infname = indir + '\\' + infname
-    #
-    #     pdf_reader = PyPDF2.PdfFileReader(infname)
-    #     pdf_writer = PyPDF2.PdfFileWriter()
-    #
-    #     lastNum = pdf_reader.getNumPages()
-    #
-    #     for i in range(0, lastNum, 2):
-    #         if i >= lastNum - 1:
-    #             if lastNum % 2 == 0:
-    #                 pdf_writer.addPage((pdf_reader.getPage(i + 1)))
-    #                 pdf_writer.addPage((pdf_reader.getPage(i)))
-    #             else:
-    #                 pdf_writer.addPage((pdf_reader.getPage(i)))
-    #             break
-    #         else:
-    #             pdf_writer.addPage((pdf_reader.getPage(i + 1)))
-    #             pdf_writer.addPage((pdf_reader.getPage(i)))
-    #
-    #     with open(outfname, "wb") as f:
-    #         pdf_writer.write(f)
-
-    def shougi(self):
-        pdfFile = 'E:\Amazon\Pdf\将棋\急所を直撃！とっておきの雁木破り.pdf'
-        pdfdir = 'E:\Amazon\Pdf\将棋'
-        os.chdir(pdfdir)
-        print(list(Path(pdfdir).glob('./*.pdf')))
-
-        # pdf24pk = Pdf24()
-        for f in list(Path(pdfdir).glob('./*.pdf')):
-            self.make_pdf24_file2dir(f, 'E:\Amazon\PDF24')
 
 if __name__ == '__main__':
 
     pdf24pk = Pdf24()
-    if True:
-        pdf24pk.shougi()
-    # else:
+    one_file = True
+    if one_file:
+        pdf24pk.make_pdf24_file2file('E:\Amazon\Pdf\将棋\急所を直撃！とっておきの雁木破り.pdf', 'E:\Amazon\PDF24\work')
+    else:
+        pdf24pk.make_pdf24_dir2dir('E:\\Amazon\\Pdf\\将棋', 'E:\\Amazon\\PDF24\\work')
     #     pdfFile = 'E:\Amazon\Pdf\将棋\急所を直撃！とっておきの雁木破り.pdf'
     #     pdfdir = 'E:\Amazon\Pdf\将棋'
     #     os.chdir(pdfdir)
@@ -174,4 +116,77 @@ if __name__ == '__main__':
 
     # def make_pdf240(self, in_fname, out_dir):
     #     self.make_pdf24(os.path.dirname(in_fname), in_fname, self.get_outfile(in_fname))
+
+   # def make_pdf24_file2dir(self, infile, outdir):
+    #     '''
+    #     indirにあるすべてのPDFファイルを変換する。
+    #     :param indir: PDFのもとファイルがあるディレクトリー
+    #     :param outdir: 出力用ディレクトリー
+    #     :return:
+    #     '''
+    #     outfile = outdir + '\\' + self.infile2outfile(infile)
+    #     self.make_pdf24_file2file(infile, outfile)
+
+   # def get_path_fname(self, infile):
+    #     '''
+    #     ロングファイル名から、ディレクトリーをファイル名を返す
+    #     :param infile: ロングファイル名
+    #     :return: ディレクトリーをファイル名
+    #     '''
+    #     dir_name = os.path.dirname(infile)
+    #     file_name = os.path.basename(infile).split('.pdf')[0]
+    #     return dir_name, file_name
+
+    # def make_pdf24_file2dir(self, infname, outdir):
+    #     '''
+    #     infname=PDFファイルをoutdirに書き込む
+    #     :param infname:
+    #     :param outdir:
+    #     :return:
+    #     '''
+    #     items = self.get_path_fname(infname)
+    #     indir = items[0]
+    #     filename= items[1] + '.pdf'
+    #     print(indir ,' ', filename)
+    #     outfilename = items[1].split('.')[0]  + '_pdf24.pdf'
+    #
+    #     self.make_pdf24_file2file(indir, outdir, filename, outfilename)
+
+    # def make_pdf24file(self, indir, outdir, infname, outfname):
+    #
+    #     # os.chdir(inPath)
+    #     outfname = outdir + '\\' + outfname
+    #     if os.path.exists(outfname):
+    #         os.remove(outfname)
+    #     infname = indir + '\\' + infname
+    #
+    #     pdf_reader = PyPDF2.PdfFileReader(infname)
+    #     pdf_writer = PyPDF2.PdfFileWriter()
+    #
+    #     lastNum = pdf_reader.getNumPages()
+    #
+    #     for i in range(0, lastNum, 2):
+    #         if i >= lastNum - 1:
+    #             if lastNum % 2 == 0:
+    #                 pdf_writer.addPage((pdf_reader.getPage(i + 1)))
+    #                 pdf_writer.addPage((pdf_reader.getPage(i)))
+    #             else:
+    #                 pdf_writer.addPage((pdf_reader.getPage(i)))
+    #             break
+    #         else:
+    #             pdf_writer.addPage((pdf_reader.getPage(i + 1)))
+    #             pdf_writer.addPage((pdf_reader.getPage(i)))
+    #
+    #     with open(outfname, "wb") as f:
+    #         pdf_writer.write(f)
+
+    # def shougi(self):
+    #     pdfFile = 'E:\Amazon\Pdf\将棋\急所を直撃！とっておきの雁木破り.pdf'
+    #     pdfdir = 'E:\Amazon\Pdf\将棋'
+        # os.chdir(pdfdir)
+        # print(list(Path(pdfdir).glob('./*.pdf')))
+        #
+        # # pdf24pk = Pdf24()
+        # for f in list(Path(pdfdir).glob('./*.pdf')):
+        #     self.make_pdf24_file2dir(f, 'E:\Amazon\PDF24')
 

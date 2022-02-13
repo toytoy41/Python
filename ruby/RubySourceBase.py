76bbb6790f5e7344068c6644e167b7e038134ff4
HP-2018\toyto <toytoy@da2.so-net.ne.jp>
2022-02-14 08:57:42 +0900

    ルビ　改良　変更しないもの、変更数　設定

--- ruby/RubySourceBase.py
+++ ruby/RubySourceBase.py
@@ -23,16 +23,26 @@ fileDic = {1:'K0.txt', 2:'K10.txt', 3:'K11.txt', 4:'K12.txt', 5:'K121.txt',
            11:'K22.txt', 12:'K23.txt', 14:'K31.txt', 15:'K32.txt',
            16:'K41.txt', 20:'K51.txt',
            21:'K52.txt', 22:'K61.txt', 23:'K62.txt', 24:'K71.txt', 25:'K72.txt',
-           26:'KOrikomi.txt', 27:'KFront.txt'}
+           26:'KOrikomi.txt', 27:'KFront.txt', 30:'nav.txt'}
 
 #　42，43，44　は　41に含まれる
 #　30はルビなし
+# るび　発展形
+# ファイルの中のキー毎に
+# '道'
+# ①　 変更するもの出現回をリストにする。
+# [1,2,5]
+# ②　 変更の対象外
+escapeDic = {3:{'道':[[1,2,5],['道じゅん','この道','ある道','鉄橋','道を','道は','道だ']],
+	'橋':[[1],['鉄橋']]}
+}
+
+CHANGENUM = 2
 
 indir = './in/'
 outdir = './out/'
 originLines = ''
-fileNumbers = [1,2]
-rubyDict ={}
+# rubyDict ={}
 
 class PutRuby():
     inFile = ''
@@ -47,13 +57,24 @@ class PutRuby():
         # self.num = 1
         pass
 
+    def morethanOne(self, nums):
+        '''
+        複数の処理番号から、ひとつづつ処理する
+        :param nums: 　処理番号
+        :return:
+        '''
+        for num in nums:
+            print(num)
+            self.SourceBase(num)
+
     def getFile(self, fileNumber):
         '''
         1ファイルのルビ辞書Dictを作成する
         :param fileNumber:
         :return:''
         '''
-        # fileNumber=1
+
+        # print(fileDic[fileNumber])
         with open(fileDic[fileNumber], 'rt', encoding='utf-8') as file:
             newline_break = ""
             for readline in file:
@@ -65,29 +86,34 @@ class PutRuby():
         line_strip0 = items.replace('{', '').replace('}', '')
         line_strip1 = line_strip0.replace(' ', '', 200)
 
-        #   Dictデータ取得
-        dictItems = line_strip1.split(',')
+        if len(line_strip1) == 0:
+            return(filename, {})
+        else:
+            #   Dictデータ取得
+            dictItems = line_strip1.split(',')
+            kanjiDict = {}
+            for term in dictItems:
+                # print(term)
+                term2 = term.replace(' ', '', 100)
+                # print(term2)
+                (k, v) = term2.split(':')
+                if k is None or v is None:
+                    pass
+                else:
+                    kanjiDict[k] = v
 
-        kanjiDict = {}
-        for term in dictItems:
-            # print(term)
-            term2 = term.replace(' ', '', 100)
-            # print(term2)
-            (k, v) = term2.split(':')
-            if k == '' or v == '':
-                pass
-            else:
-                kanjiDict[k] = v
-
-        # print(kanaDict)
-        return filename, kanjiDict
+            # print(kanaDict)
+            return filename, kanjiDict
 
     def SourceBase(self, fileNumber):
         filename, kanjiDict = self.getFile(fileNumber)
         # print('filename : ' + filename)
-
         self.inFile = indir + filename
         self.outFile = outdir + filename
+
+        if kanjiDict == {}:
+            shutil.copyfile(self.inFile, self.outFile)
+            return(0)
 
         with open(self.inFile, 'rt', encoding='utf-8') as f:
             originLines = f.readlines()
@@ -95,7 +121,6 @@ class PutRuby():
         fout = open(self.tmpFile, 'wt', encoding='utf-8')
 
         startFlg = False
-
         for inLine in originLines:
             newline = inLine
             if startFlg == False:
@@ -103,18 +128,27 @@ class PutRuby():
                 if re.search('<body>', newline):
                     startFlg = True
             else:
-                con2=0
+                # con=0
                 for kanji in kanjiDict.keys():
+                    # if con >= CHANGENUM:
+                    #     print('con >= CHANGENUM:')
+                    #     print(CHANGENUM, con)
+                    #     continue
+
                     if re.search(kanji, newline):
-                        if re.search('<rb>' + kanji + '</rb>', newline):
-                            print("no" + newline, end="")
-                        else:
+
+                        if re.search('<rb>*' + kanji + '</rb>', newline) is None\
+                                and re.search('<rb>' + kanji + '*</rb>', newline) is None:
                             kana = kanjiDict.get(kanji)
                             newline = re.sub(kanji,
                                          '<ruby> <rb>' + kanji + '</rb> <rp>（</rp> <rt>' + kana + '</rt> <rp>）</rp> </ruby>',
                                          newline)
-                            # print("yes" + new, end="")
+                            # con += 1
+                            # print("yes" + newline, end="")
                             # print(new)
+                        else:
+                            # print("no" + newline, end="")
+                            pass
 
             fout.write(newline)
 
@@ -123,7 +157,11 @@ class PutRuby():
 
 def go():
     ruby = PutRuby()
-    ruby.SourceBase(fileNumber=2)
+    # fnums = [1,2, 3,4,5,7,8,9,10,11,12,30]
+    fnums = [3]
+    # fnums = [7,8,9,10,11,12,]
+    ruby.morethanOne(fnums)
+    # ruby.SourceBase(fileNumber=2)
 
 if __name__ == '__main__':
 
